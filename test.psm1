27030d8c248aa8acbbc255da6ca1023b38728e42
Colby Bedell <bedell.colby@gmail.com>
2024-12-25 18:24:20 -0800

    Adding some functions

--- test.psm1
+++ test.psm1
@@ -1,1 +1,41 @@
+###############################################################################
+# sysadmin-toolbox module                                                     #
+# Author
+# Colby Bedell
+#
+###############################################################################
+
+
+function Load-SysAdminToolBox { 
+    param (
+       [string]$path 
+       
+    )
+    
+}
+
+
+function STB-Getfilehash {
+    param (
+        [string]$path
+    )
+    $hash = Get-FileHash -Path $path -Algorithm SHA256
+    return $hash
+}
+
+function STB-GetFileSize {
+    param (
+        [string]$path
+    )
+    $size = (Get-Item $path).length
+    return $size
+}
+
+
+function STB-GetUsers {
+    param (
+        [OptionalParameters]
+    )
+    
+}
 
